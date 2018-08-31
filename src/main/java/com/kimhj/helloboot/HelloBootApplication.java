3ad985b2ef66cde53e5aa1d0bd203173951491c9
kimhj <kimhj@gaia3d.com>
2018-08-31 21:19:38 +0900

    Oauth2 -> uri 별 인증 처리 (Token이 필요한 페이지, 그렇지 않은 페이지)

--- src/main/java/com/kimhj/helloboot/HelloBootApplication.java
+++ src/main/java/com/kimhj/helloboot/HelloBootApplication.java
@@ -3,13 +3,17 @@ package com.kimhj.helloboot;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.web.client.ResourceAccessException;
 
 // @EnableResourceServer
 // 모든 API가 OAuth 인증을 하도록 함
@@ -23,10 +27,17 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 // 둘다 받아야 서버 동작함
 @EnableAuthorizationServer
 @SpringBootApplication
-public class HelloBootApplication {
+public class HelloBootApplication extends ResourceServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloBootApplication.class, args);
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/member/**").permitAll()
+				.antMatchers("/posts/**").authenticated();
 	}
 	
 	@Bean
